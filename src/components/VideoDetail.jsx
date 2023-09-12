5774083cbf2507f846208708e4069875fef97ba1
Saurav Thakur <63474029+sauravthakur01@users.noreply.github.com>
2023-09-13 00:15:46 +0530

    added search functionality

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { useState , useEffect} from 'react'
+import { useParams ,Link } from 'react-router-dom'
+import ReactPlayer from 'react-player'
+import { Typography , Box , Stack } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+
+import { Videos } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const VideoDetail = () => {
   return (
